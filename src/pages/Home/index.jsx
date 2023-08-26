324a9d02d41ed69b85526cd621fe42a7008454aa
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-08-26 17:00:12 +0700

    Add gluestack ui

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+function Home() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({});
